cee8949eae0f6c9c5b89e212a402a63c7312a2fa
FFTacc <rohan.kamath.24@gmail.com>
2026-02-13 10:36:30 +0530

    Added bit reversal, updated tcl script, butterfly still under check

--- project_start.tcl
+++ project_start.tcl
@@ -4,7 +4,7 @@ set project_dir "./Vivado_Workspace"
 create_project $project_name $project_dir -part xc7a35tcpg236-1 -force
 
 # 2. Add your existing Verilog sources
-add_files [glob ./src/*.v]
+add_files [glob ./verilog_sources/*.v]
 
 # 4. Set a module as 'Top' for analysis (since you don't have a top-level yet)
 set_property top multiplier [current_fileset]
