9deb44a959d99069c588bbcdbbc450cbf5391fd9
FFTacc <rohan.kamath.24@gmail.com>
2026-02-16 22:52:20 +0530

    Supposed final mixed precision wrapper for butterfly and fft template in progress...

--- verilog_sources/mixed_precision_wrappers.v
+++ verilog_sources/mixed_precision_wrappers.v
@@ -7,16 +7,21 @@ module butterfly_wrapper #(
     input [23:0] A, B,  // 24-bit unified format inputs
     input [15:0] W,     // twiddle factor (16-bit for FP8, only [7:0] used for FP4)
     output [15:0] X, Y  // 24-bit unified format outputs
+    output output_is_fp8;
 );
     generate
         if (MULT_PRECISION == 0 && ADD_PRECISION == 0) begin : USE_PURE_FP4
             fp4_butterfly_generation_unit fp4_butterfly_inst(.A(A[7:0]), .B(B[7:0]), .W(W), .X({8'h00, X[7:0]}), .Y({8'h00, Y[7:0]}));
+            output_is_fp8 = 1'b0;
         end else if (MULT_PRECISION == 1 && ADD_PRECISION == 1) begin : USE_PURE_FP8
             fp8_butterfly_generation_unit fp8_butterfly_inst(.A(A[23:8]), .B(B[23:8]), .W(W), .X(X), .Y(Y));
+            output_is_fp8 = 1'b1;
         end else if (MULT_PRECISION == 0 && ADD_PRECISION == 1) begin: USE_FP8add_FP4mul
             butterfly_generation_unit_8add_4mul fp4mul_fp8add_inst(.A(A[23:8]), .B(B[7:0]), .W(W), .X(X), .Y(Y));
+            output_is_fp8 = 1'b1;
         end else if (MULT_PRECISION == 1 && ADD_PRECISION == 0) begin: USE_FP8mul_FP4add
             butterfly_generation_unit_4add_8mul fp8mul_fp4add_inst(.A(A[7:0]), .B(B[23:8]), .W(W), .X({8'h00, X[7:0]}), .Y({8'h00, Y[7:0]}));
+            output_is_fp8 = 1'b0;
         end
     endgenerate
 endmodule
